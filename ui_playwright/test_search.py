24ddf0347f91924c542b70bd6816c3b38aa129ad
Josh Esquivel <josh.esquivel1@gmail.com>
2024-05-02 21:30:51 -0700

    Adding playwright files

--- ui_playwright/test_search.py
+++ ui_playwright/test_search.py
@@ -1,6 +1,7 @@
 from pageobjects.search import SearchPage
 
-# test file
+# test file - you still want page objects so that your test(s) can easily call them
+# also your page objects are responsible for the selectors while tests are for actual validation
 page = await browser.new_page()
 search_page = SearchPage(page)
 await search_page.navigate()
